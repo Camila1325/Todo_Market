3b2e0205d2d4f7809890c92acce73dd8aa0d9f53
Camila1325 <kamirodriguez25@gmail.com>
2026-04-25 17:04:10 -0500

    Creación de la página de Authenticación

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Market from './pages/Market';
+import Auth from './pages/Auth';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/market" element={<Market />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   );
 }
