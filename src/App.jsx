f6100c292eb8e6906f78590e87423a964bdb6df5
Camila1325 <kamirodriguez25@gmail.com>
2026-04-26 03:38:47 -0500

    Corregir importaciones duplicadas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
